d844d9b04acdee56667c10430aa288d80855a9b3
Parveen Kumar <parveen.kumar6@gmail.com>
2020-07-29 16:06:32 -0700

    cpp

--- compcpp/cpp_comp_template.cpp
+++ compcpp/cpp_comp_template.cpp
@@ -8,6 +8,7 @@
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 
 using namespace std;
@@ -20,5 +21,13 @@ typedef vector<int> vi;
 #define INF 1000000000 //1B
 
 int main(){
+    //quick calculations
+    //ans = a ? b : c; // to simplify: if(a) ans = b else ans = c
+    //ans += val; // to simplify: ans = ans + val;
+    //index = (index + 1) % n; // index++; if (index >=n) index = 0;
+    //index = (index + n - 1) % n; // index--; if (index < 0) index = n-1;
+    //int ans = (int)((double)d + 0.5); // for rounding to nearest integer
+    //ans = min(ans, new_computation); //min/max shortcut
+    //
 
 }
