e24e5a4c4b4983b4f49c75e21035a9a39ba044f4
Parveen Kumar <parveen.kumar6@gmail.com>
2020-12-17 17:27:51 +0530

    archive 12/17/2020

--- archive_program/base/data_structures_and_libs/01_array_vector.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int main(){
-	int arr[5] = {7,7,7}; // initial size is 5 and initial values {7,7,7,0,0}
-	vector<int> v (5,5);  // initial size is 5 and initial values {5,5,5,5,5}
-	cout<<"arr[2]  "  << arr[2] << " " << "v[2] " << v[2] << "\n";
-	return 0;
-}
