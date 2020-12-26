e5235bce86b704439e8635cd28323cda9ef4ebcb
Parveen Kumar <parveen.kumar6@gmail.com>
2020-12-26 21:00:09 +0530

    class and object

--- cpplang/oops/01_class_and_object.cpp
+++ cpplang/oops/01_class_and_object.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+class Test{
+public:
+    int testNo;
+    char testName[20];
+    void attempt(){
+        cout << "Test is attempted!" <<testName <<" " <<testNo << "\n";
+    }
+};
+
+int main(){
+    Test t;
+    t.testNo = 321;
+    t.testName[0] = 't';
+    t.testName[1] = 'e';
+    t.testName[2] = 's';
+    t.testName[3] = 't';
+    t.testName[4] = '1';
+    t.testName[5] = '\0';
+    t.attempt();
+    return 0;
+}
